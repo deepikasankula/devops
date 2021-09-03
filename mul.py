2f0a3aef55bd8ad8f611d3512c01e7ee9528b0dc
DEEPIKA SANKULA <4769-deepika381@users.noreply.code.kluniversity.com>
2021-09-03 22:21:06 +0530

    first commit

---
+++ mul.py
@@ -0,0 +1,10 @@
+def multiplyList(myList):
+Result =1
+For Xin my list:
+Result = result*x
+Return result
+#driver code
+List1 = [1,2,3]
+List2 = [3,2,4]
+print(multiplyList(list1))
+print(multiplyList(list2))
